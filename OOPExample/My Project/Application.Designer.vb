eee4baa19b17226e107f998dc6631afbe5b97676
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-10-22 10:43:08 -0600

    Basic Set up for Card Objects

--- OOPExample/My Project/Application.Designer.vb
+++ OOPExample/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.OOPExample.Form1
+            Me.MainForm = Global.OOPExample.OOPExample
         End Sub
     End Class
 End Namespace
---
+++ OOPExample/OOPExample.vb
@@ -0,0 +1,19 @@
+﻿Imports System.ComponentModel
+
+Public Class OOPExample
+    Private Sub OOPExample_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub OOPExample_Click(sender As Object, e As EventArgs) Handles Me.Click
+        Dim ah As New Card("a", "h")
+        'Dim cardName As String = "_AS"
+        'Dim cardImage As Image = My.Resources.ResourceManager.GetObject(cardName)
+        Dim g As Graphics = Me.CreateGraphics
+
+        g.DrawImage(ah.backImage, 10, 10)
+        g.DrawImage(ah.frontImage, 100, 100)
+
+        g.Dispose()
+    End Sub
+End Class
