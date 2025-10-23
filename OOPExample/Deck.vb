941a3af9907cd43fe4e4d4531b32214f2191faf1
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-10-23 09:43:11 -0600

    Added a deal card function as well as CardsLeft

--- OOPExample/Deck.vb
+++ OOPExample/Deck.vb
@@ -1,3 +1,39 @@
 ﻿Public Class Deck
+    Private _deck As New Stack(Of Card)
+
+    Sub test()
+        Dim ah As New Card("a", "h")
+        Dim asp As New Card("a", "s")
+        Dim ac As New Card("a", "c")
+        Dim ad As New Card("a", "d")
+        Me._deck.Push(ah) 'Push Adds a card to the stack
+        Me._deck.Push(asp)
+        Me._deck.Push(ac)
+        Me._deck.Push(ad)
+    End Sub
+
+    Sub Shuffle()
+
+    End Sub
+
+    ''' <summary>
+    ''' Deals one card from the top of the card Stack
+    ''' </summary>
+    ''' <returns>Card</returns>
+    Function DealCard() As Card
+        Return _deck.Pop 'Pop will remove a card from the stack
+    End Function
+
+    ''' <summary>
+    ''' Returns the number of cards you have left
+    ''' </summary>
+    ''' <returns>Integer</returns>
+    Function CardsLeft() As Integer
+        Return _deck.Count
+    End Function
+
+    Sub New()
+        test()
+    End Sub
 
 End Class
--- OOPExample/OOPExample.vb
+++ OOPExample/OOPExample.vb
@@ -1,18 +1,26 @@
 ﻿Imports System.ComponentModel
 
 Public Class OOPExample
+    Dim testDeck As New Deck 'put this in GameLogic Class
     Private Sub OOPExample_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
     Private Sub OOPExample_Click(sender As Object, e As EventArgs) Handles Me.Click
-        Dim ah As New Card("a", "h")
+        'Dim ah As New Card("a", "h")
         'Dim cardName As String = "_AS"
         'Dim cardImage As Image = My.Resources.ResourceManager.GetObject(cardName)
+        'Dim currentCard As New Card
+
+        'testDeck.DealCard()
+
         Dim g As Graphics = Me.CreateGraphics
 
-        g.DrawImage(ah.backImage, 10, 10)
-        g.DrawImage(ah.frontImage, 100, 100)
+        g.DrawImage(testDeck.DealCard.frontImage, 10, 10)
+        'g.DrawImage(testDeck.DealCard.frontImage, 50, 50)
+        'g.DrawImage(testDeck.DealCard.frontImage, 100, 100)
+        'g.DrawImage(testDeck.DealCard.frontImage, 150, 150)
+        'g.DrawImage(ah.frontImage, 100, 100)
 
         g.Dispose()
     End Sub
